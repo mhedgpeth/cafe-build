0fb14806f644c47a05d7b009f6fa829aad8a8fc6
Annie Hedgpeth <annie@hedge-ops.priv>
2017-01-24 09:36:29 -0600

    master includes Docker cookbook

--- jenkins-master.rb
+++ jenkins-master.rb
@@ -4,13 +4,13 @@
 # https://docs.chef.io/policyfile.html
 
 # A name that describes what the system you're building with Chef does.
-name "jenkins-master"
+name 'jenkins-master'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list "cafe-build::master"
+run_list 'cafe-build::master'
 
 # Specify a custom source for a single cookbook:
-cookbook "cafe-build", path: "."
+cookbook 'cafe-build', path: '.'
--- recipes/master.rb
+++ recipes/master.rb
@@ -1,4 +1,15 @@
-# docker
-# jenkins
-# run container with port forwarding and disk mapping
-# use docker cookbook
+cookbook 'docker'
+
+docker_service 'jenkins' do
+  action [:create, :start]
+end
+
+docker_image 'jenkins' do
+  action :pull
+end
+
+docker_container 'jenkins' do
+  repo 'jenkins'
+  port ['8080:8080', '50000:50000']
+  volumes ['/your/home:/var/jenkins_home jenkins']
+end
